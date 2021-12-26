e80b419f0805d40d3ffa6e1d091f15c764cedff8
o.sukhryn <o.sukhryn@temabit.com>
2021-12-26 13:08:36 +0200

    add redis

---
+++ redis.go
@@ -0,0 +1,49 @@
+package redis
+
+import (
+	"fmt"
+	"github.com/go-redis/redis"
+	"strconv"
+	"log"
+)
+
+type RedisConnectionParams struct {
+	URL, Password, Host, Port, DB string
+}
+
+func convertStringToInt(s string) int {
+	result, err := strconv.Atoi(s)
+	if err != nil {
+		log.Fatal("Not parse int")
+	}
+	return result
+}
+
+func BuildRedisOptions(p *RedisConnectionParams) *redis.Options {
+	return &redis.Options{
+		Addr:     fmt.Sprintf("%s:%s", p.Host, p.Port),
+		Password: p.Password,
+		DB:       convertStringToInt(p.DB),
+	}
+}
+
+func CheckAlive(c *redis.Client) bool {
+	_, err := c.Ping().Result()
+	if err != nil {
+		log.Fatal("Error connect to Redis")
+		return false
+	}
+	return true
+}
+
+func Connect(p *redis.Options) *redis.Client {
+	client := redis.NewClient(p)
+
+	statusOK := CheckAlive(client)
+
+	if !statusOK{
+		panic("Redis not alive")
+	} 
+	
+	return client
+}
